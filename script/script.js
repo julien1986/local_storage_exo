a9985e5fce4fdebd31cb5a1c5c64487ef3a64cbe
ingrwf <ingrwf@cpg-453069.cepegra2008.lan>
2019-06-26 11:30:03 +0200

    git init

---
+++ script/script.js
@@ -0,0 +1,45 @@
+const myForm = document.querySelector("form");
+
+//récup des données du localStorage
+if (localStorage.quidam) {
+  var quidam = JSON.parse(localStorage.quidam);
+  for (property in quidam) {
+    if (property != "commande") {
+      if (document.querySelector(`[name=${property}]`)) {
+        document.querySelector(`[name=${property}]`).value = quidam[property];
+      }
+    } else {
+      commande = quidam[property];
+
+      if (document.querySelector(`input[value='${commande}']`)) {
+        document.querySelector(`input[value='${commande}']`).checked = "checked";
+        //console.log(document.querySelector(`input[value='${fruit}']`))
+      }
+    } //command
+  }
+}
+
+//Alimenttion du localStorage
+myForm.addEventListener("submit", function(event) {
+  event.preventDefault();
+  page = myForm.dataset.page;
+  //alert(page)
+
+  let formData = new FormData(myForm);
+
+  //si il y a du localStorage, on en garde le contenu
+  if (typeof quidam != "undefined") {
+    dataJson = quidam;
+  } else {
+    // pas de localStorage, on crée un objet vide
+    dataJson = new Object();
+  }
+
+  for (value of formData.entries()) {
+    dataJson[value[0]] = value[1];
+  }
+  localStorage.quidam = JSON.stringify(dataJson);
+  if (page == 1) {
+    location.replace("commande.html");
+  }
+});
